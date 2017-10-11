3544c9c9d00b9d91904218d7619c102aadf09365
SriHarshaGajavalli <sriharsha.g15@iiits.in>
2017-10-11 18:48:00 +0530

    Added backaward compatibility

--- home/models.py
+++ home/models.py
@@ -10,34 +10,23 @@ import datetime
 
 @python_2_unicode_compatible
 class Roles(models.Model):
-<<<<<<< HEAD
+
     Role_ID = models.AutoField(primary_key=True)
     Role_name = models.CharField(max_length=50, default="")
     level = models.IntegerField(default=0)
 
     @property
-=======
-    Role_ID=models.AutoField(primary_key=True)
-    Role_name=models.CharField(max_length=50,default="")
-    level=models.IntegerField(default=0)
->>>>>>> origin/master
     def __str__(self):
         return self.Role_name
 
 @python_2_unicode_compatible
 class Personnel(models.Model):
-<<<<<<< HEAD
     Person_ID = models.IntegerField(primary_key=True)
     LDAP = models.OneToOneField(User, on_delete=models.CASCADE)
     Role = models.ForeignKey(Roles, to_field='Role_ID',
                              on_delete=models.CASCADE)  # Make sure whether this has to be foreign key
 
     @property
-=======
-    Person_ID=models.IntegerField(primary_key=True)
-    LDAP=models.OneToOneField(User, on_delete=models.CASCADE)
-    Role=models.ForeignKey(Roles,to_field='Role_ID',on_delete=models.CASCADE)#Make sure whether this has to be foreign key
->>>>>>> origin/master
     def __str__(self):
         return self.LDAP.username
 
@@ -56,12 +45,9 @@ class Department(models.Model):
 
     #	Head_ID=models.ForeignKey('Personnel',to_field='Person_ID',on_delete=models.CASCADE)
 
-<<<<<<< HEAD
     # Head_ID=models.ForeignKey('Personnel',to_field='Person_ID',on_delete=models.CASCADE)
     @property
-=======
-    #Head_ID=models.ForeignKey('Personnel',to_field='Person_ID',on_delete=models.CASCADE)
->>>>>>> origin/master
+
     def __str__(self):
         return self.Dept_Name
 
@@ -75,32 +61,21 @@ class Courses(models.Model):
     # Inst_ID=models.ForeignKey(Personnel,to_field='Person_ID',on_delete=models.CASCADE)
     Course_description = models.CharField(max_length=255, default="")
     Course_Credits = models.IntegerField()
-<<<<<<< HEAD
+
     Course_Status = models.BooleanField(default=True)
 
     # Course_Status = models.BooleanField(default= True)
     @property
-=======
-    Course_Status = models.BooleanField(default = True)
-    #Course_Status = models.BooleanField(default= True)
->>>>>>> origin/master
     def __str__(self):
         return self.Course_Name
 
 
 class Attendance(models.Model):
-<<<<<<< HEAD
     Student_ID = models.ForeignKey(Personnel, to_field='Person_ID', on_delete=models.CASCADE)
     Course_ID = models.ForeignKey(Courses, to_field='Course_ID', on_delete=models.CASCADE)
     Date_time = models.DateTimeField(default=datetime.datetime.now())
     Marked = models.CharField(default="A", max_length=1)
 
-=======
-    Student_ID=models.ForeignKey(Personnel,to_field='Person_ID',on_delete=models.CASCADE)
-    Session_ID=models.ForeignKey('Attendance_Session',to_field='Session_ID',blank=True,null=True)
-    Date_time=models.DateTimeField(default=datetime.datetime.now())
-    Marked=models.CharField(default="A",max_length=1)
->>>>>>> origin/master
 
 
 class Attendance_Session(models.Model):
@@ -137,26 +112,18 @@ class Assignment(models.Model):
         return str(self.Assign_ID)
 
 class Submissions(models.Model):
-<<<<<<< HEAD
+
     # Sub_ID=db.MultiFieldPK('Assign_ID','Student_ID')
 
-    Sub_ID = models.AutoField(primary_key=True)
-=======
     Sub_ID=models.AutoField(primary_key=True)
     Assign_ID=models.ForeignKey(Assignment,to_field='Assign_ID',on_delete=models.CASCADE)
     Student_ID=models.ForeignKey(Personnel,to_field='Person_ID',on_delete=models.CASCADE)
     Sub_Time=models.DateTimeField(default=utils.timezone.now)
     Score=models.FloatField(default=0)
->>>>>>> origin/master
-
-    Assign_ID = models.ForeignKey(Assignment, to_field='Assign_ID', on_delete=models.CASCADE)
-    Student_ID = models.ForeignKey(Personnel, to_field='Person_ID', on_delete=models.CASCADE)
-    Sub_Time = models.DateTimeField(default=utils.timezone.now)
-    Score = models.FloatField(default=0)
 
 @python_2_unicode_compatible
 class Instructors_Courses(models.Model):
-<<<<<<< HEAD
+
     IC_id = models.AutoField(primary_key=True)
     Course_ID = models.ForeignKey(Courses, to_field='Course_ID', on_delete=models.CASCADE)
     Inst_ID = models.ForeignKey(Personnel, to_field='Person_ID', on_delete=models.CASCADE)
@@ -164,19 +131,13 @@ class Instructors_Courses(models.Model):
     End_Date = models.DateField(datetime.date(2017, 1, 1))
 
     @property
-=======
-    IC_id=models.AutoField(primary_key=True)
-    Course_ID=models.ForeignKey(Courses,to_field='Course_ID',on_delete=models.CASCADE)
-    Inst_ID=models.ForeignKey(Personnel,to_field='Person_ID',on_delete=models.CASCADE)
-    Start_Date=models.DateField(datetime.date(2017,1,1))
-    End_Date=models.DateField(datetime.date(2017,1,1))
->>>>>>> origin/master
+
     def __str__(self):
         return str((self.Inst_ID))
 
 @python_2_unicode_compatible
 class Students_Courses(models.Model):
-<<<<<<< HEAD
+
     # SC_ID=db.MultiFieldPK('Student_ID','Course_ID')
     SC_ID = models.AutoField(primary_key=True)
     Student_ID = models.ForeignKey(Personnel, to_field='Person_ID', on_delete=models.CASCADE)
@@ -184,12 +145,6 @@ class Students_Courses(models.Model):
     Reg_Date = models.DateField(datetime.date(2017, 1, 1))
 
     @property
-=======
-    SC_ID=models.AutoField(primary_key=True)
-    Student_ID=models.ForeignKey(Personnel,to_field='Person_ID',on_delete=models.CASCADE)
-    Course_ID=models.ForeignKey(Courses,to_field='Course_ID',on_delete=models.CASCADE)
-    Reg_Date=models.DateField(datetime.date(2017,1,1))
->>>>>>> origin/master
     def __str__(self):
         return str(self.Student_ID) + ' ' + str(self.Course_ID)
 
@@ -201,15 +156,10 @@ class Events(models.Model):
 
 
 class Student_Period(models.Model):
-<<<<<<< HEAD
     Student_ID = models.ForeignKey(Personnel, to_field='Person_ID')
     Start_Year = models.IntegerField(default=2013)
     End_Year = models.IntegerField(default=2017)
-=======
-    Student_ID=models.ForeignKey(Personnel,to_field='Person_ID')
-    Start_Year=models.IntegerField(default=2013)
-    End_Year=models.IntegerField(default=2017)
-
+@python_2_unicode_compatible
 class Timetable(models.Model):
     T_ID=models.AutoField(primary_key=True)
     DAYS_OF_WEEK = (
@@ -226,6 +176,6 @@ class Timetable(models.Model):
     End_time=models.TimeField(auto_now_add=True)
     Course_ID=models.ForeignKey(Courses,to_field='Course_ID',on_delete=models.CASCADE)
     Class_ID=models.CharField(max_length=10,default='')
+    @property
     def __str__(self):
         return self.T_ID
->>>>>>> origin/master

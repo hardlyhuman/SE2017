d381c26c4827bcd12826dc8a1d1292832d6bc2f8
SriHarshaGajavalli <sriharsha.g15@iiits.in>
2017-10-16 22:25:01 +0530

    Removed +1 duplicate

--- students/urls.py
+++ students/urls.py
@@ -4,10 +4,6 @@ from . import views
 urlpatterns = [
         url(r'^$',views.dashboard,name='dashboard'),
         url(r'^viewattendance/', views.ViewAttendance, name='ViewAttendance'),
-        #url(r'^CourseRegistration/', views.CourseRegistration, name='CourseRegistration'),
-        url(r'^AddCourse/', views.AddCourse, name='AddCourse'),
-        #url(r'^DropCourse/', views.DropCourse, name='DropCourse'),
-        #url(r'^MarkAttendance/', views.MarkAttendance, name='MarkAttendance'),
         url(r'^assgnsubstatus/', views.AssgnSubStatus, name='AssgnSubmissionStatus'),
         url(r'^register_course/', views.register_course, name='register_course'),
         url(r'^register/', views.register.as_view(), name='register'),

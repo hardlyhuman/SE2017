4409c5f2c70579946df3bd2d39cac43909028e44
SriHarshaGajavalli <sriharsha.g15@iiits.in>
2017-10-16 21:45:07 +0530

    Removed trash

--- students/views.py
+++ students/views.py
@@ -46,31 +46,6 @@ def ViewAttendance(request, StuId):
 def dashboard(request):
     pass
 
-""""@login_required(login_url="/login/")
-def CourseRegistration(request, ):
-
-    
-    course = None
-    form_class = CourseEnrollForm
-    
-    return render(request, 'student/CourseRegistration.html', {'courses': c})"""
-"""
-class StudentEnrollCourseView(LoginRequiredMixin, FormView):
-    course = None
-    form_class = CourseEnrollForm
-
-
-    def form_valid(self, form):
-        self.course = form.cleaned_data['course']
-        self.course.students.add(self.request.user)
-        return super(StudentEnrollCourseView,
-                     self).form_valid(form)
-
-    @property
-    def get_success_url(self):
-        return reverse_lazy('student_course_detail',
-                            args=[self.course.id])
-"""
 @login_required(login_url="/login/")
 class register(FormView):
 
@@ -132,58 +107,13 @@ def unregister(request):
 
 
 
-def MarkAttendance(request):
-    return render(request, 'student/MarkAttendance.html')
 
 
 def AddCourse(request):
     return render(request, 'student/AddCourse.html')
 
 
-def DropCourse(request):
-    return render(request, 'student/DropCourse.html')
 
-class ViewAttendance(TemplateView):
-    template_name = 'student/ViewAttendance.html'
-
-    def get(self, request, *args, **kwargs):
-        try:
-            if request.user.personnel.Role.Role_name == "student":
-                person_id = request.user.personnel.Person_ID
-            my_courses_list = [i.Course.course_title for i in person_id.Students_Courses_set.all()]
-            courses_registered = [model_to_dict(Students_Courses.objects.get(course_title=i)) for i in my_courses_list]
-
-            dic = {}
-            absent_on = [[i.course_title, i.date] for i in
-                         Attendance.objects.filter(student_id=person_id.Stu).filter(status="A")]
-
-            # Making a dictionary { Course: [date1, date2, ....], .... }
-            for c in absent_on:
-                if c[0] not in dic:
-                    dic[c[0]] = [c[1]]
-                else:
-                    dic[c[0]] += [c[1]]
-
-            # Converting date to a dic as follows - { Month1: [day1, day2, .. ], ..... }
-            month_names = ["January", "February", "March", "April", "May", "June", "July", "August", "September",
-                           "October", "November", "December"]
-            for course in dic:
-
-                month_dic = {}
-                for date in dic[course]:
-
-                    t_month = month_names[date.month - 1]
-                    if t_month not in month_dic:
-                        month_dic[t_month] = [date]
-                    else:
-                        month_dic[t_month] += [date]
-                dic[course] = month_dic
-
-            context = {'courses_registered': courses_registered,  'absent_on': dic}
-
-            return render(request, self.template_name, context)
-        except:
-            return render(request, self.template_name, {'error_message': 'No data found'})
 
 
 def AssgnSubStatus(request, StuId):

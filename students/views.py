2763bc8b8430ada3090ece3a45665da08c4c0053
SriHarshaGajavalli <sriharsha.g15@iiits.in>
2017-10-17 17:01:05 +0530

    Minor fix - views.py

--- students/views.py
+++ students/views.py
@@ -3,7 +3,7 @@
 #__author__ = "Sri Harsha Gajavalli"
 #__copyright__ = "Copyright 2017, SE2017 Course"
 #__Team__ = ["Sri Harsha Gajavalli", "Koushik Bharadwaj", "David Christie", "Likhith Lanka", "Sajas P", "Rajeev Reddy"]
-#__license__ = "GPL"
+#__license__ = "MIT"
 #__version__ = "1.2"
 #__maintainer__ = "Likhith Lanka"
 #__email__ = "likhith.l15@iiits.in"

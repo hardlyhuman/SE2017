5d621bdaabfa9091bad5e5edd243936db96059f7
SriHarshaGajavalli <sriharsha.g15@iiits.in>
2017-10-17 16:53:22 +0530

    Added File Header

--- students/views.py
+++ students/views.py
@@ -1,3 +1,16 @@
+###################################################
+# students/views.py: Comprises of all the controllers for students app of SAMS-IIITS
+#__author__ = "Sri Harsha Gajavalli"
+#__copyright__ = "Copyright 2017, SE2017 Course"
+#__Team__ = ["Sri Harsha Gajavalli", "Koushik Bharadwaj", "David Christie", "Likhith Lanka", "Sajas P", "Rajeev Reddy"]
+#__license__ = "GPL"
+#__version__ = "1.2"
+#__maintainer__ = "Likhith Lanka"
+#__email__ = "likhith.l15@iiits.in"
+#__status__ = "Development"
+####################################################
+
+
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 

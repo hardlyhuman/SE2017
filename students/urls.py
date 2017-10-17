5bbf77ed88e474da32427fb272d2c0c69819f182
SriHarshaGajavalli <sriharsha.g15@iiits.in>
2017-10-17 17:02:00 +0530

    Added File Header - urls.py

--- students/urls.py
+++ students/urls.py
@@ -1,3 +1,16 @@
+###################################################
+# students/urls.py: Consists of list of urls of students app of SAMS-IIITS
+#__author__ = "Sri Harsha Gajavalli"
+#__copyright__ = "Copyright 2017, SE2017 Course"
+#__Team__ = ["Sri Harsha Gajavalli", "Koushik Bharadwaj", "David Christie", "Likhith Lanka", "Sajas P", "Rajeev Reddy"]
+#__license__ = "MIT"
+#__version__ = "1.2"
+#__maintainer__ = "Likhith Lanka"
+#__email__ = "likhith.l15@iiits.in"
+#__status__ = "Development"
+####################################################
+
+
 # -*- coding: utf-8 -*-
 from django.conf.urls import url
 from . import views

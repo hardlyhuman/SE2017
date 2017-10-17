4f8d707a0423187da9e7532d12823813aad2582b
Vaishnav Murali <vaishnavmurali@gmail.com>
2017-10-17 23:18:17 +0530

    herku set up

--- SE2017/urls.py
+++ SE2017/urls.py
@@ -26,8 +26,6 @@ urlpatterns = [
 	url(r'^$', include('home.urls'), name='home'),
     url(r'^login/',  auth_views.login, {'template_name': 'Login/login.html'}, name='login'),
 	url(r'^logout/$', auth_views.logout, {'template_name': 'Login/login.html'}, name='logout'),
-	url(r'^student/', include('students.urls'), name='student'),
-	url(r'^faculty/', include('faculty.urls'), name='faculty'),
-    url(r'^admin/', admin.site.urls),
+	url(r'^admin/', admin.site.urls),
     url(r'^api/',include('home.urls')),
 ]

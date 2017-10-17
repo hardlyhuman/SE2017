2b830d14abdeb31b7aef918df03e5df29808fe4d
Vaishnav Murali <vaishnavmurali@gmail.com>
2017-10-17 22:57:31 +0530

    herku set up

--- SE2017/wsgi.py
+++ SE2017/wsgi.py
@@ -7,7 +7,11 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 """
 
+import os
+
 from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "SE2017.settings")
+
 application = get_wsgi_application()
 application = DjangoWhiteNoise(application)

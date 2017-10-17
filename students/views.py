b57e2e67306ea9433535cbd7f002b91bcdd2d240
SriHarshaGajavalli <sriharsha.g15@iiits.in>
2017-10-17 16:44:03 +0530

    Cleared Trash

--- students/views.py
+++ students/views.py
@@ -13,21 +13,7 @@ from home.models import *
 
 # Create your views here.
 
-"""
-def ViewRegCourse(request):
-    CourseList = []
-    if request.user.personnel.Role.Role_name == "student":
-        person_id = request.user.personnel.Person_ID
-        SC = Students_Courses.objects.all()
-        for i in range(0, len(SC)):
-            if person_id == SC[i].Student_ID.Person_ID:
-                CourseList.append(SC[i].Course_ID.Course_Name)
-
-    template = loader.get_template('student/dashboard.html')
-    context = dict(Course=json.dumps(CourseList), Stud_Name=request.user.username)
-    return HttpResponse(template.render(context, request))"""
-"""
-
+'''
 def ViewAttendance(request, StuId):
     CourseId = 0
     Total = 25
@@ -38,7 +24,7 @@ def ViewAttendance(request, StuId):
             if each['Marked'] == 'P':
                 present += 1
     return render(request, 'student/ViewAttendance.html', {'Total': Total, 'present': present, 'Absent': Total - present, 'percent': (present/Total)*100})
-"""
+'''
 def dashboard(request):
     pass
 

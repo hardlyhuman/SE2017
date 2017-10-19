0d7d3747129b315ba4239021b0e6f2686bdaf604
Vaishnav Murali <vaishnavmurali@gmail.com>
2017-10-19 16:09:12 +0530

    data dump

--- home/urls.py
+++ home/urls.py
@@ -33,5 +33,7 @@ urlpatterns = [
 	url(r'^add_view_attendance_sessions/',views.add_view_attendance_sessions),
 	url(r'^add_view_timetable/',views.add_view_timetable),
 	url(r'^validate_user/',views.validate_user),
+    url(r'^student_rel_courses/',views.student_rel_courses),
+	url(r'^faculty_rel_courses/',views.faculty_rel_courses),
 
 ]

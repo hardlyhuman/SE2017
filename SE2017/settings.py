b822629cb6cb47535b28f292c59b3460bb3ea9ac
SriHarshaGajavalli <sriharsha.g15@iiits.in>
2017-10-20 22:24:05 +0530

    mail added

--- SE2017/settings.py
+++ SE2017/settings.py
@@ -105,9 +105,9 @@ else:
         'default': {
             'ENGINE': 'django.db.backends.mysql',
             'HOST': 'localhost',
-            'NAME': 'se',
+            'NAME': 'se2017',
             'USER': 'root',
-            'PASSWORD': 'PASSWORD',
+            'PASSWORD': '5qlDevelop#r',
 
 
         }

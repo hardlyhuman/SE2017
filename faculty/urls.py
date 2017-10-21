399c19d15bffe65296abd3ff900756c78d96d991
Savitha <savithag1197@gmail.com>
2017-10-21 10:47:14 +0530

    faculty updated (#54) (#55)
    * faculty app added
    
    * final app
    
    * new
    
    * added email scheduler.
    
    * Small Addition
    
    * Small progress
    
    * Added CourseRegistration Base
    
    * Added DropCourse Base
    
    * Added DropCourse
    
    * Added Course Registration urls
    
    * +1 Commit
    
    * Removed trash
    
    * CourseRegistration done
    
    * Minor Fix
    
    * remove ViewCourses function
    
    * Removed Duplicate urls
    
    * Removed +1 duplicate
    
    * Optimised imports
    
    * Optimised imports done
    
    * Added MyCourses view
    
    * Started ViewAttendance
    
    * Minor Fix
    
    * Modified EndPoint View
    
    * removed Useless Endpoint view
    
    * Modified Dashboard
    
    * Modified Dashboard
    
    * Added AddCourse Endpoint View
    
    * Added DropCourse Endpoint View
    
    * Added MyCourses Endpoint View
    
    * Cleared Trash
    
    * Added File Header
    
    * Create LICENSE
    
    * Minor fix - views.py
    
    * Added File Header - urls.py
    
    * Added File Headers - Student app
    
    * Added File Header for main urls.py
    
    * Cleared useless comments
    
    * minor Fix
    
    * Modified Attendance view
    
    * common template
    
    * fixed templates
    
    * Modified Attendance view (#48)
    
    * Template Done. (#49)
    
    * added email scheduler.
    
    * common template
    
    * fixed templates
    
    * Update .gitignore
    
    * dist
    
    * Bootsrap dist (#51)
    
    * added email scheduler.
    
    * common template
    
    * fixed templates
    
    * dist
    
    * templates addition
    
    * student courses api added
    
    * API added (#52)
    
    * added email scheduler.
    
    * common template
    
    * fixed templates
    
    * dist
    
    * student courses api added
    
    * Update models.py
    
    * Fixes
    
    * faculty app
    
    * faculty app

--- faculty/urls.py
+++ faculty/urls.py
@@ -1,19 +1,13 @@
 from django.conf.urls import url
-
 from . import views
-
 app_name='faculty'
-urlpatterns =[
-	
-         url(r'^ViewProfs/$',views.ViewProfs,name='ViewProfs'),   
-url(r'^ViewRegisteredStudents/$',views.ViewRegisteredStudents,name='ViewRegisteredStudents'), 
+urlpatterns =[	
+         url(r'^ViewProfs/$',views.ViewProfs,name='ViewProfs'),
+ url(r'^ViewAttendance/$',views.ViewAttendance,name='ViewAttendance'),    
 url(r'^AddAssignment/$',views.AddAssignment,name='AddAssignment'),   
 url(r'^Delass/$',views.Delass,name='Delass'),  
-url(r'^delete/$',views.delete,name='delete'), 
-url(r'^offercourses/$',views.OfferCourses,name='OfferCourses'), 
-url(r'^editdescription/$',views.EditCourseDescription,name='EditCourseDescription'), 
-        
-        
-	
-    ]
+url(r'^offercourses/$',views.OfferCourses,name='OfferCourses'),  
+url(r'^coursepage/$',views.CoursePage,name='CoursePage'),
+url(r'^index/$',views.index,name='index'),      
+]
 

5fb34da90eb11635b6bf6a995bb70a8bfc2c6082
SriHarshaGajavalli <sriharsha.g15@iiits.in>
2017-10-26 15:00:48 +0530

    student links added

--- students/urls.py
+++ students/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import url
 from . import views
-
+app_name='students'
 urlpatterns = [
         url(r'^$',views.dashboard,name='index'),
         url(r'^viewattendance/', views.viewattendance , name='ViewAttendance'),

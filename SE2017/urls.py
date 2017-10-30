b3894d833b0a40ce5d0df9a9125ebdaa3ece416d
SriHarshaGajavalli <sriharsha.g15@iiits.in>
2017-10-30 10:18:20 +0530

    Login - All changes done

--- SE2017/urls.py
+++ SE2017/urls.py
@@ -41,7 +41,7 @@ urlpatterns = [
     url(r'^login/',  auth_views.login, {'template_name': 'Login/login.html'}, name='login'),
 	url(r'^logout/$', auth_views.logout, {'template_name': 'Login/login.html'}, name='logout'),
 	url(r'^admin/', admin.site.urls),
-    url(r'^students/', include('students.urls'), name='students'),
+    url(r'^student/', include('students.urls'), name='students'),
     url(r'^faculty/', include('faculty.urls'), name='faculty'),
     url(r'^api/',include('home.urls')),
 ]

abb856968ffd58684373a8c963e557f2fabb1219
Shariq03 <shariqsuhail.m15@iiits.in>
2017-11-04 13:18:16 +0530

    Update views.py

--- home/views.py
+++ home/views.py
@@ -39,7 +39,7 @@ def haversine(lon1, lat1, lon2, lat2):
     lon1, lat1, lon2, lat2 = map(radians, [lon1, lat1, lon2, lat2])
 
 
-    
+    #harvesine formula
     dlon = lon2 - lon1
     dlat = lat2 - lat1
     a = sin(dlat/2)**2 + cos(lat1) * cos(lat2) * sin(dlon/2)**2

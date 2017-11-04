02be0cc596decb1c445a25e3226aba9d50de5b7d
Shariq03 <shariqsuhail.m15@iiits.in>
2017-11-04 14:16:42 +0530

    Update forms.py

--- home/forms.py
+++ home/forms.py
@@ -11,21 +11,3 @@ class PersonnelForm(forms.ModelForm):
         fields = ('Person_ID','Role','Dept')
 
 #Form for taking input from user for email frequency
-class ProfileForm(forms.ModelForm):
-	username = forms.CharField(label = 'dd')
-	def __init__(self, *args, **kwargs):
-		self.user = kwargs.pop('user')
-		self.name = self.user.username
-		super(ProfileForm, self).__init__(*args, **kwargs)
-		self.fields['username'] = forms.CharField(label = 'Username',initial = self.name, disabled = 'disabled')
-
-	class Meta:
-		model = NotificationTime
-		fields = ('Notification_time',)
-
-	def save(self, commit=True):
-		obj = Personnel.objects.get(LDAP_id=self.user.id)
-		personID = obj.Person_ID
-		personObj = Personnel.objects.get(Person_ID=personID)
-		obj_ , created = NotificationTime.objects.update_or_create(Personnel_ID=personObj,defaults={'Notification_time':self.cleaned_data.get('Notification_time')})
-		return obj_

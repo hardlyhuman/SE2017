71f26a722ae53f40c4024639293c5c8e27bfaf93
vagdevik <vagdevi.k15@iiitsin>
2017-11-09 14:34:41 +0530

    commit

--- faculty/templatetags/faculty_extras.py
+++
@@ -1,8 +0,0 @@
-from django import template
-from django.template.defaultfilters import *
-
-register = template.Library()
-
-@register.filter(name='multiply')	
-def multiply(value,arg):
-	return value*arg

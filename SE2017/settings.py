759371da9ef609f99ccb9400d8d885729703e5dd
savitha97 <savithag1197@gmail.com>
2017-11-15 22:41:32 +0530

    CSS Partially fixed for Faculty

--- SE2017/settings.py
+++ SE2017/settings.py
@@ -122,9 +122,9 @@ else:
         'default': {
             'ENGINE': 'django.db.backends.mysql',
             'HOST': 'localhost',
-            'NAME': 'se2017',
+            'NAME': 'se',
             'USER': 'root',
-            'PASSWORD': '5qlDevelop#r',
+            'PASSWORD': 'PASSWORD',
 
             }
 
